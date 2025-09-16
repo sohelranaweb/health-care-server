0fc2a4d6ef73e7b0e3367b3fe149f6ff10052eea
Shafayathub <shafayat.ms@gmail.com>
2025-09-16 18:41:39 +0600

    starter file

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+
+const app: Application = express();
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+        Message: "Ph health care server.."
+    })
+});
+
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
+
+export default app;
